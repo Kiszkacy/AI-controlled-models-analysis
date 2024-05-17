eb171d1494f5e8f94940b10be17d3aab4ce2e4d7
Kiszkacy <karolkisz22@gmail.com>
2024-05-17 15:34:43 +0200

    add NeatPrint to enumerable objects

---
+++ engine/src/scripts/global/extensions/EnumerableExtensions.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+
+public static class EnumerableExtensions
+{
+	public static void NeatPrint(this IEnumerable enumerable)
+	{
+		NeatPrinter printer = NeatPrinter.Start().Print("[\n");
+		foreach (var item in enumerable)
+		{
+			printer.Print("\t").Print((item ?? "null").ToString()).Print(",\n");
+		}
+		printer.Print("]").End();
+	}
+}
