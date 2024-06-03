a5175dd69116ae976d94043c91c70c5b9d6a4a28
Kiszkacy <karolkisz22@gmail.com>
2024-06-03 23:25:12 +0200

    change food color randomly on ready

--- engine/src/scripts/environment/food/Food.cs
+++ engine/src/scripts/environment/food/Food.cs
@@ -1,7 +1,14 @@
+using System;
 using Godot;
 
 public partial class Food : Node2D
 {
 	[Export] 
 	public float EnergyNutrition { get; set; } = 5;
+
+	public override void _Ready()
+	{
+		Sprite2D sprite = this.GetNode<Sprite2D>("Sprite");
+		sprite.Modulate = Color.FromHsv((float)(new Random().NextDouble() * 0.1f), (float)(new Random().NextDouble() * 0.5f + 0.5f), 1.0f);;
+	}
 }
