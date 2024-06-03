aa8a466664f66e2e29db529927a62fc1fd16a087
Kiszkacy <karolkisz22@gmail.com>
2024-06-03 23:49:16 +0200

    decrease food alpha

--- engine/src/scripts/environment/food/Food.cs
+++ engine/src/scripts/environment/food/Food.cs
@@ -9,6 +9,6 @@ public partial class Food : Node2D
 	public override void _Ready()
 	{
 		Sprite2D sprite = this.GetNode<Sprite2D>("Sprite");
-		sprite.Modulate = Color.FromHsv((float)(new Random().NextDouble() * 0.1f), (float)(new Random().NextDouble() * 0.5f + 0.5f), 1.0f);;
+		sprite.Modulate = Color.FromHsv((float)(new Random().NextDouble() * 0.1f), (float)(new Random().NextDouble() * 0.5f + 0.5f), 1.0f, 0.75f);
 	}
 }
