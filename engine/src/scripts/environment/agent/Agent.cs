79c9218b5d6b589e337f1e3969f7cfdbaf707e6f
Kiszkacy <karolkisz22@gmail.com>
2024-06-05 17:52:28 +0200

    add agent control methods & redo movementProcess

--- engine/src/scripts/environment/agent/Agent.cs
+++ engine/src/scripts/environment/agent/Agent.cs
@@ -1,11 +1,15 @@
 using Godot;
-using System;
-using System.Collections.Generic;
 
 public partial class Agent : CharacterBody2D
 {
 	[Export]
-	public float MaximumSpeed { get; set; } = 200.0f; // in px
+	public float MaximumSpeed { get; set; } = 200.0f; // in px per sec
+	
+	[Export]
+	public float MaximumAcceleration { get; set; } = 100.0f; // in px per sec
+	
+	[Export]
+	public float MaximumDeceleration { get; set; } = 100.0f; // in px per sec
 	
 	[Export]
 	public float MaximumEnergy { get; set; } = 200.0f;
@@ -20,20 +24,22 @@ public partial class Agent : CharacterBody2D
 	public float InitialHealth { get; set; } = 100.0f;
 	
 	[Export]
-	public float MaximumTurnSpeedRadiansPerSecond { get; set; } = 6.0f;
+	public float MaximumTurnSpeed { get; set; } = Mathf.Pi / 2.0f; // in radians per sec
 	
 	[Export]
-	public float SightAngleInRadians { get; set; } = Mathf.Pi / 4.0f;
+	public float SightAngle { get; set; } = Mathf.Pi / 4.0f; // in radians
 	
 	[Export]
 	public float SightRadius { get; set; } = 256.0f; // in px
 		
 	private float energy;
 	private float health;
-	private float rotateRadiansPerSecond = 0.0f;
+	private float currentRotation = 0.0f;
+	private float currentAcceleration = 0.0f;
 
 	public Vector2 Direction { get; set; } = Vector2.Right;
 	public float DirectionAngle => this.Direction.Angle();
+	public float Speed => this.Velocity.Length();
 
 	private Food closestFood = null;
 	public float DistanceToClosestFood => this.closestFood?.GlobalPosition.DistanceTo(this.GlobalPosition) ?? float.NaN;
@@ -75,13 +81,23 @@ public partial class Agent : CharacterBody2D
 
 			Vector2 directionToFood = (food.GlobalPosition - this.GlobalPosition).Normalized();
 			float angleToFoodInRadians = this.Direction.AngleTo(directionToFood);
-			if (Mathf.Abs(angleToFoodInRadians) < this.SightAngleInRadians / 2.0f)
+			if (Mathf.Abs(angleToFoodInRadians) < this.SightAngle / 2.0f)
 			{
 				this.closestFood = food;
 			}
 		}
 	}
 
+	private void Rotate(float strength) // input of range <-1, 1>
+	{
+		this.currentRotation = Mathf.Remap(strength, -1, 1, -this.MaximumTurnSpeed, this.MaximumTurnSpeed);
+	}
+	
+	private void Accelerate(float strength) // input of range <-1, 1>
+	{
+		this.currentAcceleration = Mathf.Remap(strength, -1, 1, -this.MaximumDeceleration, this.MaximumAcceleration);
+	}
+	
 	private void onMouthBodyEntered(Node2D body)
 	{
 		if (body is not Food food) return;
@@ -92,9 +108,19 @@ public partial class Agent : CharacterBody2D
 	
 	private void MovementProcess(double delta)
 	{
-		this.GlobalRotation += (float)delta * this.rotateRadiansPerSecond;
-		float angle = Math.GodotToRad(this.GlobalRotation);
-		this.Velocity = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * this.Velocity.Length();
+		this.Direction = this.Direction.Rotated(this.currentRotation * this.MaximumTurnSpeed * (float)delta);
+		this.GlobalRotation = this.DirectionAngle;
+		
+		if (this.currentAcceleration >= 0.0f)
+		{
+			this.Velocity = this.Direction * Mathf.Clamp((this.Speed + this.currentAcceleration) * this.MaximumAcceleration * (float)delta, 0.0f, this.MaximumSpeed);
+		}
+		else
+		{
+			this.Velocity = this.Direction * Mathf.Clamp((this.Speed + this.currentAcceleration) * this.MaximumDeceleration * (float)delta, 0.0f, this.MaximumSpeed);
+		}
+		
+			
 		this.MoveAndCollide(this.Velocity);
 	}
 
