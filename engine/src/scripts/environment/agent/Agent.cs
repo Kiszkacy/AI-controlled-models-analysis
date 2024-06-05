2b88588061a74d0d478222afb58275f182c7f575
Kiszkacy <karolkisz22@gmail.com>
2024-06-05 16:33:28 +0200

    update agent script

--- engine/src/scripts/environment/agent/Agent.cs
+++ engine/src/scripts/environment/agent/Agent.cs
@@ -1,10 +1,11 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class Agent : CharacterBody2D
 {
 	[Export]
-	public float MaximumSpeed { get; set; } = 500.0f;
+	public float MaximumSpeed { get; set; } = 200.0f; // in px
 	
 	[Export]
 	public float MaximumEnergy { get; set; } = 200.0f;
@@ -13,7 +14,7 @@ public partial class Agent : CharacterBody2D
 	public float InitialEnergy { get; set; } = 50.0f;
 	
 	[Export]
-	public float MaximumHealth { get; set; } = 200.0f;
+	public float MaximumHealth { get; set; } = 100.0f;
 	
 	[Export]
 	public float InitialHealth { get; set; } = 100.0f;
@@ -23,16 +24,16 @@ public partial class Agent : CharacterBody2D
 	
 	[Export]
 	public float SightAngleRadians { get; set; } = 2.0f;
+	
+	[Export]
+	public float SightRadius { get; set; } = 256.0f; // in px
 		
 	private float energy;
 	private float health;
 	private float rotateRadiansPerSecond = 0.0f;
 	
-	public float getPointingAngle()
-	{
-		return Velocity.Angle();
-	}
-	
+	public float GetPointingAngle() => this.Velocity.Angle();
+
 	private void UpdateEnergy(double delta)
 	{
 		this.energy -= Config.Instance.Data.Environment.EnergyLossPerSecond * (float)delta;
@@ -40,7 +41,22 @@ public partial class Agent : CharacterBody2D
 	
 	private void UpdateHealth(double delta)
 	{
-		this.health -= Config.Instance.Data.Environment.HealthLossPerSecond * (float)delta;
+		if (this.energy <= 0.0f) // TODO proper float comparison
+		{
+			this.health -= Config.Instance.Data.Environment.HealthLossPerSecond * (float)delta;
+		}
+		else
+		{
+			this.health += Config.Instance.Data.Environment.HealthRegenPerSecond * (float)delta;
+		}
+	}
+
+	private void MovementProcess(double delta)
+	{
+		this.GlobalRotation += (float) delta * rotateRadiansPerSecond;
+		float angle = Math.GodotToRad(this.GlobalRotation);
+		this.Velocity = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * Velocity.Length();
+		this.MoveAndCollide(Velocity);
 	}
 	
 	public override void _Ready()
@@ -56,16 +72,8 @@ public partial class Agent : CharacterBody2D
 
 	public override void _PhysicsProcess(double delta)
 	{
-		if (this.energy > 0.0f)
-		{
-			this.UpdateEnergy(delta);
-		}
-		else 
-		{
-			this.UpdateHealth(delta);
-		}
-		this.GlobalRotation += (float) delta * rotateRadiansPerSecond;
-		this.Velocity = new Vector2(Mathf.Cos(Rotation), Mathf.Sin(Rotation)) * Velocity.Length();
-		MoveAndCollide(Velocity);
+		this.UpdateEnergy(delta);
+		this.UpdateHealth(delta);
+		this.MovementProcess(delta);
 	}
 }
