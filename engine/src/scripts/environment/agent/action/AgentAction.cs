707882caf70e0699c03adcd7462db36688d83a25
Kiszkacy <karolkisz22@gmail.com>
2024-06-06 13:49:28 +0200

    add agentAction struct

---
+++ engine/src/scripts/environment/agent/action/AgentAction.cs
@@ -0,0 +1,14 @@
+﻿
+public struct AgentAction
+{
+    public int Id { get; }
+    public float AccelerateStrength { get; }
+    public float RotateStrength { get; }
+
+    public AgentAction(int id, float accelerateStrength, float rotateStrength)
+    {
+        this.Id = id;
+        this.AccelerateStrength = accelerateStrength;
+        this.RotateStrength = rotateStrength;
+    }
+}
