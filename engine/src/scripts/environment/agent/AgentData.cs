e5e9ea1176da951cf25577e20ed2cb0fb35c8b40
Kiszkacy <karolkisz22@gmail.com>
2024-06-06 14:41:02 +0200

    fix agentData NaN data mapping

--- engine/src/scripts/environment/agent/AgentData.cs
+++ engine/src/scripts/environment/agent/AgentData.cs
@@ -27,8 +27,8 @@ public readonly struct AgentData
             speed: Mathf.Remap(this.Speed, 0.0f, agent.MaximumSpeed, -1, 1),
             energy: Mathf.Remap(this.Energy, 0.0f, agent.MaximumEnergy, -1, 1),
             health: Mathf.Remap(this.Health, 0.0f, agent.MaximumHealth, -1, 1),
-            distanceToClosestFood: Mathf.Remap(this.DistanceToClosestFood, 0.0f, agent.SightRadius, -1, 1),
-            angleToClosestFood: Mathf.Remap(this.AngleToClosestFood, -agent.SightAngle/2.0f, agent.SightAngle/2.0f, -1, 1)
+            distanceToClosestFood: Mathf.Remap(float.IsNaN(this.DistanceToClosestFood) ? -999 : this.DistanceToClosestFood, 0.0f, agent.SightRadius, -1, 1),
+            angleToClosestFood: Mathf.Remap(float.IsNaN(this.AngleToClosestFood) ? -999 : this.AngleToClosestFood, -agent.SightAngle/2.0f, agent.SightAngle/2.0f, -1, 1)
         );
     }
 
