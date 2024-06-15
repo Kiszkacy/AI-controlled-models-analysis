08c11f2159f033b28afcddfcec578fc5270b5cc6
MasterHamster42 <115225781+MasterHamster42@users.noreply.github.com>
2024-06-15 14:31:00 +0200

    Add type hints to setup.py

--- core/src/setup.py
+++ core/src/setup.py
@@ -1,11 +1,12 @@
 import inspect
 import logging
 import sys
+from types import FrameType
 
 from loguru import logger
 
 
-def configure_logging():
+def configure_logging() -> None:
     class InterceptHandler(logging.Handler):
         def emit(self, record: logging.LogRecord) -> None:
             # Get corresponding Loguru level if it exists.
@@ -16,7 +17,8 @@ def configure_logging():
                 level = record.levelno
 
             # Find caller from where originated the logged message.
-            frame, depth = inspect.currentframe(), 0
+            frame: FrameType | None = inspect.currentframe()
+            depth: int = 0
             while frame and (depth == 0 or frame.f_code.co_filename == logging.__file__):
                 frame = frame.f_back
                 depth += 1
