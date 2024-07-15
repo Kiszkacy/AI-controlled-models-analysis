edd7006ec5a694012dc6a11af1d98117fae1650b
Kiszkacy <karolkisz22@gmail.com>
2024-07-15 21:20:00 +0200

    add timer tests

---
+++ engine/src/scripts/global/timer/Timer.Test.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework;
+
+public class TimerTest : TestClass<TimerTest>
+{
+    [Test]
+    public void IsInactive_BeforeActivation()
+    {
+        // given
+        Timer timer = new(this.MockAction);
+
+        // then
+        Assert.That(timer.IsActive, Is.False);
+    }
+
+    [Test]
+    public void IsActive_AfterActivation()
+    {
+        // given
+        Timer timer = new(this.MockAction);
+
+        // when
+        timer.Activate(10.0);
+
+        // then
+        Assert.That(timer.IsActive, Is.True);
+    }
+
+    [Test]
+    public void IsInactive_AfterTimeout()
+    {
+        // given
+        Timer timer = new(this.MockAction);
+
+        // when
+        timer.Activate(10.0);
+        TestUtility.SimulateProcess(timer, 10.0);
+
+        // then
+        Assert.That(timer.IsActive, Is.False);
+    }
+
+    [Test]
+    public void ActionCalled_AfterTimeout()
+    {
+        // given
+        bool actionCalled = false;
+        void MockLocalAction()
+        {
+            actionCalled = true;
+        }
+        Timer timer = new(MockLocalAction);
+
+        // when
+        timer.Activate(10.0);
+        TestUtility.SimulateProcess(timer, 10.0);
+
+        // then
+        Assert.That(actionCalled, Is.True);
+    }
+
+    [Test]
+    public void ActionNotCalled_BeforeTimeout()
+    {
+        // given
+        bool actionCalled = false;
+        void MockLocalAction()
+        {
+            actionCalled = true;
+        }
+        Timer timer = new(MockLocalAction);
+
+        // when
+        timer.Activate(10.0);
+        TestUtility.SimulateProcess(timer, 5.0);
+
+        // then
+        Assert.That(actionCalled, Is.False);
+    }
+
+    private void MockAction() { }
+}
