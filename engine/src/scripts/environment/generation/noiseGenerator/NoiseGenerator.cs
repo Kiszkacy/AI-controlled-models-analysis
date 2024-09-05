cf2a054a19f1d4115b367884a001b0dbb7af8725
Kiszkacy <karolkisz22@gmail.com>
2024-09-05 20:46:51 +0200

    add noise generator

---
+++ engine/src/scripts/environment/generation/noiseGenerator/NoiseGenerator.cs
@@ -0,0 +1,22 @@
+﻿
+using Godot;
+
+public class NoiseGenerator
+{
+    private readonly FastNoiseLite noise;
+
+    public float At(Vector2 position)
+    {
+        return this.noise.GetNoise2D(position.X, position.Y);
+    }
+    
+    public float At(float positionX, float positionY)
+    {
+        return this.noise.GetNoise2D(positionX, positionY);
+    }
+
+    public NoiseGenerator(FastNoiseLite fastNoiseLite)
+    {
+        this.noise = fastNoiseLite;
+    }
+}
---
+++ engine/src/scripts/environment/generation/noiseGenerator/NoiseGeneratorBuilder.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+
+using Godot;
+
+public class NoiseGeneratorBuilder
+{
+    private readonly FastNoiseLite noise = new();
+    
+    public static NoiseGeneratorBuilder Start => new();
+
+    public NoiseGeneratorBuilder SetNoiseType(FastNoiseLite.NoiseTypeEnum type)
+    {
+        this.noise.NoiseType = type;
+        return this;
+    }
+
+    public NoiseGeneratorBuilder SetSeed(int seed)
+    {
+        this.noise.Seed = seed;
+        return this;
+    }
+
+    public NoiseGeneratorBuilder SetFrequency(float frequency) {
+        this.noise.Frequency = frequency;
+        return this;
+    }
+    
+    public NoiseGeneratorBuilder SetOctaves(int octaves) {
+        this.noise.FractalOctaves = octaves;
+        return this;
+    }
+    
+    public NoiseGeneratorBuilder SetGain(float gain) {
+        this.noise.FractalGain = gain;
+        return this;
+    }
+
+    public NoiseGenerator End()
+    {
+        return new NoiseGenerator(this.noise);
+    }
+
+    public NoiseGenerator SetAllToDefault()
+    {
+        return this.SetNoiseType(FastNoiseLite.NoiseTypeEnum.Perlin)
+            .SetSeed(new Random().Next())
+            .SetFrequency(0.01f)
+            .SetOctaves(4)
+            .SetGain(0.5f)
+            .End();
+    }
+    
+    private NoiseGeneratorBuilder() {}
+}
