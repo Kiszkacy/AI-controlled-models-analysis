dc4712cd2a29a2b8245ddcbd240ed8f4397f17ca
Kiszkacy <karolkisz22@gmail.com>
2024-09-19 14:18:12 +0200

    add generationUtil

---
+++ engine/src/scripts/simulation/environment/generation/generators/util/EnvironmentGenerationUtil.cs
@@ -0,0 +1,25 @@
+﻿
+using Godot;
+
+public static class EnvironmentGenerationUtil
+{
+    public static BiomeType GetBiomeAt(Vector2 position, Vector2 size, Vector2 biomeChunkSize, BiomeType[] biomeData)
+    {
+        int biomeChunksInARow = (int)(size.X / biomeChunkSize.X) + (size.X % biomeChunkSize.X != 0 ? 1 : 0);
+        
+        int targetBiomeChunkColumnIndex = (int)(position.X / biomeChunkSize.X);
+        int targetBiomeChunkRowIndex = (int)(position.Y / biomeChunkSize.Y);
+        
+        return biomeData[targetBiomeChunkRowIndex * biomeChunksInARow + targetBiomeChunkColumnIndex];
+    }
+    
+    public static bool IsTerrainAt(Vector2 position, Vector2 size, Vector2 terrainChunkSize, bool[] terrainData)
+    {
+        int terrainChunksInARow = (int)(size.X / terrainChunkSize.X) + (size.X % terrainChunkSize.X != 0 ? 1 : 0);
+        
+        int targetTerrainChunkColumnIndex = (int)(position.X / terrainChunkSize.X);
+        int targetTerrainChunkRowIndex = (int)(position.Y / terrainChunkSize.Y);
+
+        return terrainData[targetTerrainChunkRowIndex * terrainChunksInARow + targetTerrainChunkColumnIndex];
+    }
+}
