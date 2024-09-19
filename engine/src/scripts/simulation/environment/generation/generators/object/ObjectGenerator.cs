f16161bd3ba3467b9d1c641a1846b2d310023856
Kiszkacy <karolkisz22@gmail.com>
2024-09-19 14:18:39 +0200

    add objectGenerator

---
+++ engine/src/scripts/simulation/environment/generation/generators/object/ObjectGenerator.cs
@@ -0,0 +1,85 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Godot;
+
+public class ObjectGenerator
+{
+    private readonly int maxAmountOfSpawnTriesIfFailed;
+    
+    public EnvironmentObjectData[] Generate(EnvironmentGenerationSettings settings, BiomeType[] biomeData, bool[] terrainData)
+    {
+        LinkedList<EnvironmentObjectData> data = new();
+        Vector2 currentChunkPosition = Vector2.Zero;
+
+        foreach (bool isTerrain in terrainData)
+        {
+            if (!isTerrain)
+            {
+                currentChunkPosition.X += settings.TerrainChunkSize.X;
+                if (currentChunkPosition.X >= settings.Size.X)
+                {
+                    currentChunkPosition.X = 0;
+                    currentChunkPosition.Y += settings.TerrainChunkSize.Y;
+                }
+                continue;
+            }
+
+            Vector2 currentChunkCenter = currentChunkPosition + settings.TerrainChunkSize / 2.0f;
+            BiomeType biomeAtCurrentPosition = EnvironmentGenerationUtil.GetBiomeAt(currentChunkCenter, settings.Size, settings.BiomeChunkSize, biomeData);
+            float targetAmountOfObjects = BiomeTable.BiomeObjectPool[biomeAtCurrentPosition].AmountOfObjectsPer1000x1000Pixels;
+            float chanceAmount = (settings.TerrainChunkSize.X * settings.TerrainChunkSize.Y / (1000 * 1000)) * targetAmountOfObjects;
+
+            while (chanceAmount > 0.0f)
+            {
+                if (chanceAmount <= 1.0f && !(new Random().NextDouble() <= chanceAmount))
+                {
+                    break;
+                }
+
+                Vector2? possiblePosition = this.GetValidObjectPosition(currentChunkPosition, settings.TerrainChunkSize);
+                if (possiblePosition.HasValue)
+                {
+                    Vector2 position = possiblePosition.Value;
+                    EnvironmentObjectId objectId = this.PickObjectId(biomeAtCurrentPosition); // TODO: replace this method call and method itself with new Random() class method
+                    data.AddLast(new EnvironmentObjectData(objectId, position));
+                }
+                chanceAmount -= 1.0f;
+            }
+
+            currentChunkPosition.X += settings.TerrainChunkSize.X;
+            if (currentChunkPosition.X >= settings.Size.X)
+            {
+                currentChunkPosition.X = 0;
+                currentChunkPosition.Y += settings.TerrainChunkSize.Y;
+            }
+        }
+        
+        return data.ToArray();
+    }
+
+    private Vector2? GetValidObjectPosition(Vector2 topLeftPosition, Vector2 chunkSize)
+    {
+        // TODO: here add missing safe space checks and spawnTries using maxAmountOfSpawnTriesIfFailed
+        // int tryCount = 0;
+        // while (tryCount < this.maxAmountOfSpawnTriesIfFailed) 
+        // {
+        //         
+        // }
+        Vector2 position = new(
+            topLeftPosition.X + (float)(new Random().NextDouble()) * chunkSize.X,
+            topLeftPosition.Y + (float)(new Random().NextDouble()) * chunkSize.Y
+        );
+
+        return position;
+    }
+
+    private EnvironmentObjectId PickObjectId(BiomeType biomeType)
+    {
+        EnvironmentObjectId[] objects = BiomeTable.BiomeObjectPool[biomeType].Objects;
+        int index = new Random().Next(objects.Length);
+        return objects[index];
+    }
+}
---
+++ engine/src/scripts/simulation/environment/generation/generators/object/ObjectGeneratorBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Godot;
+
+public class ObjectGeneratorBuilder
+{
+    public static ObjectGeneratorBuilder Start => new();
+
+    public ObjectGenerator End()
+    {
+        return new ObjectGenerator();
+    }
+
+    public ObjectGeneratorBuilder SetAllToDefault()
+    {
+        return this;
+    }
+    
+    private ObjectGeneratorBuilder() {}
+}
