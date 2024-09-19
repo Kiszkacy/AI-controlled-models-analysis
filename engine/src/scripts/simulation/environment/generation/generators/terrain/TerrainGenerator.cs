e858cefa2b1ca204ef5fe4c908921a303faa3e19
Kiszkacy <karolkisz22@gmail.com>
2024-09-19 14:18:29 +0200

    add terrainGenerator

---
+++ engine/src/scripts/simulation/environment/generation/generators/terrain/TerrainGenerator.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Godot;
+
+public class TerrainGenerator
+{
+    public bool[] Generate(EnvironmentGenerationSettings settings, BiomeType[] biomeData)
+    {
+        bool[] isIslandAccordingToBiomes = GetIslandChunksAccordingToBiomeData(settings, biomeData);
+
+        // TODO: do some steps with noise
+
+        return isIslandAccordingToBiomes;
+    }
+
+    private static bool[] GetIslandChunksAccordingToBiomeData(EnvironmentGenerationSettings settings, BiomeType[] biomeData)
+    {
+        Vector2 currentChunkPosition = Vector2.Zero;
+        LinkedList<bool> isIsland = new();
+        
+        while (true)
+        {
+            BiomeType biomeAtCurrentPosition = EnvironmentGenerationUtil.GetBiomeAt(currentChunkPosition, settings.Size, settings.BiomeChunkSize, biomeData);
+            isIsland.AddLast(biomeAtCurrentPosition != BiomeType.Ocean);
+            
+            currentChunkPosition.X += settings.TerrainChunkSize.X;
+            if (currentChunkPosition.X >= settings.Size.X)
+            {
+                currentChunkPosition.X = 0;
+                currentChunkPosition.Y += settings.TerrainChunkSize.Y;
+            }
+            if (currentChunkPosition.Y >= settings.Size.Y)
+            {
+                break;
+            }
+        }
+
+        return isIsland.ToArray();
+    }
+    
+}
---
+++ engine/src/scripts/simulation/environment/generation/generators/terrain/TerrainGeneratorBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Godot;
+
+public class TerrainGeneratorBuilder
+{
+    public static TerrainGeneratorBuilder Start => new();
+
+    public TerrainGenerator End()
+    {
+        return new TerrainGenerator();
+    }
+
+    public TerrainGeneratorBuilder SetAllToDefault()
+    {
+        return this;
+    }
+    
+    private TerrainGeneratorBuilder() {}
+}
