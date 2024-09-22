24f16ae26a81f4828f481c0b906be33218d0ef9a
Kiszkacy <karolkisz22@gmail.com>
2024-09-22 22:32:17 +0200

    make entityManager initializable

--- engine/src/scripts/simulation/entityManager/EntityManager.cs
+++ engine/src/scripts/simulation/entityManager/EntityManager.cs
@@ -1,12 +1,17 @@
 ﻿
 using Godot;
 
-public class EntityManager : Singleton<EntityManager>
+public class EntityManager : Singleton<EntityManager>, Initializable
 {
     public EntityLayer<Food> FoodBuckets { get; private set; }
+    
+    private readonly InitializableWrapper initialized = new();
+    public bool IsInitialized => this.initialized.IsInitialized;
 
-    public void Setup(Vector2 environmentSize)
+    public void Initialize(Vector2 environmentSize)
     {
         this.FoodBuckets = new EntityLayer<Food>(environmentSize);
+        this.FoodBuckets.Initialize();
+        this.initialized.Initialize();
     }
 }
