b9b4175d3a9705a468e63b19ded90bdbd77a4018
Kiszkacy <karolkisz22@gmail.com>
2024-09-25 20:02:53 +0200

    add linkedList extend method

---
+++ engine/src/scripts/global/extensions/LinkedListExtensions.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Collections.Generic;
+
+public static class LinkedListExtensions
+{
+    public static void Extend<T>(this LinkedList<T> first, LinkedList<T> second)
+    {
+        foreach (var entry in second)
+        {
+            first.AddLast(entry);
+        }
+    }
+}
