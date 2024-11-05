a8fa54a5a9a2843569b2e311d70d1468299f0613
Kiszkacy <karolkisz22@gmail.com>
2024-11-05 16:31:29 +0100

    add environmentManager

---
+++ engine/src/scripts/simulation/environment/manager/EnvironmentManager.cs
@@ -0,0 +1,32 @@
+
+using Godot;
+
+public class EnvironmentManager : Singleton<EnvironmentManager>, Initializable
+{
+    public Environment Environment { get; private set; }
+    
+    private readonly InitializableWrapper initialized = new();
+    public bool IsInitialized => this.initialized.IsInitialized;
+    
+    public void Initialize(Environment environment)
+    {
+        this.Environment = environment;
+        this.initialized.Initialize();
+    }
+
+    public bool IsTerrainAt(Vector2 where)
+    {
+        return BiomeType.Ocean != EnvironmentGenerationUtil.GetBiomeAt(where, this.Environment.Size, this.Environment.TemplateData.GenerationSettings.BiomeChunkSize, this.Environment.TemplateData.BiomeData);
+    }
+    
+    public BiomeType GetBiomeAt(Vector2 where)
+    {
+        return EnvironmentGenerationUtil.GetBiomeAt(where, this.Environment.Size, this.Environment.TemplateData.GenerationSettings.BiomeChunkSize, this.Environment.TemplateData.BiomeData);
+    }
+    
+    public void Reset()
+    {
+        this.Environment = null;
+        this.initialized.Reset();
+    }
+}
