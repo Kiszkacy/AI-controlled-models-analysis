9ac524fb7f9222da5dd0e43873516888c9075106
Kiszkacy <karolkisz22@gmail.com>
2024-11-05 16:39:15 +0100

    add simulation view ui

---
+++ engine/src/scripts/simulation/ui/minimap/Minimap.cs
@@ -0,0 +1,30 @@
+using Godot;
+
+public partial class Minimap : TileMap
+{
+    // sizeOfTileInPixels = 8 (change inside godot editor)
+    // sizeOfTileInPixels * sizeInTiles = 256 !!
+    public const int sizeInTiles = 32;
+
+    public override void _Ready()
+    {
+        this.ReloadMinimap();
+    }
+
+    private void ReloadMinimap()
+    {
+        Vector2 environmentSize = EnvironmentManager.Instance.Environment.Size;
+        Vector2 tileSize = environmentSize / sizeInTiles;
+
+        for (int y = 0; y < sizeInTiles; y++)
+        {
+            Vector2 currentPosition = new(tileSize.X / 2.0f, tileSize.Y / 2.0f + tileSize.Y * y);
+            for (int x = 0; x < sizeInTiles; x++)
+            {
+                BiomeType biomeType = EnvironmentManager.Instance.GetBiomeAt(currentPosition);
+                this.SetCell(0, new Vector2I(x, y), 0, new Vector2I((int)biomeType, 0));
+                currentPosition.X += tileSize.X;
+            }
+        }
+    }
+}
---
+++ engine/src/scripts/simulation/ui/pauseButton/PauseButton.cs
@@ -0,0 +1,29 @@
+﻿
+using Godot;
+
+public partial class PauseButton : Button
+{
+    private Texture2D iconPaused;
+    private Texture2D iconRunning;
+    private bool inPauseMode = false;
+
+    public override void _Ready()
+    {
+        this.iconPaused = (Texture2D)GD.Load("res://assets/icons/solid/play.svg");
+        this.iconRunning = (Texture2D)GD.Load("res://assets/icons/solid/pause.svg");
+    }
+
+    public void Invert()
+    {
+        this.inPauseMode = !this.inPauseMode;
+
+        if (this.inPauseMode)
+        {
+            this.Icon = this.iconPaused;
+        }
+        else
+        {
+            this.Icon = this.iconRunning;
+        }
+    }
+}
