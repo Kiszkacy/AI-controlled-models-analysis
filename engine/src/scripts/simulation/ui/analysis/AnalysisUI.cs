cf11c3a967ecf53142c55a274bd33910ac1a1932
Kiszkacy <karolkisz22@gmail.com>
2024-11-10 20:23:36 +0100

    add basic analysis ui logic

---
+++ engine/src/scripts/simulation/ui/analysis/AnalysisUI.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Linq;
+
+using Godot;
+
+public partial class AnalysisUI : Control, Observable
+{
+    [Export] 
+    public EnvironmentTracker EnvironmentTracker;
+    
+    [Export] 
+    public Chart AgentChart;
+
+    public override void _Ready()
+    {
+        EventManager.Instance.Subscribe(this, EventChannel.EnvironmentTracker);
+    }
+
+    public void Notify(IEvent @event)
+    {
+        this.AgentChart.UpdateChartData(
+            this.EnvironmentTracker.AgentsCountData.Zip(this.EnvironmentTracker.TimeData, (agentCount, time) => new Vector2((float)time, agentCount)).ToArray(),
+            new Vector2((float)this.EnvironmentTracker.TimeData.Min(), 0),
+            new Vector2((float)this.EnvironmentTracker.TimeData.Max(), 100),
+            new Vector2(5, 5)
+        );
+    }
+}
