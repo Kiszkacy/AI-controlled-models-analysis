a44645f4ab7c5ffb0a8dd2491cd63db2bbbdf8f6
Ania Nowacka <anna.nowacka146@gmail.com>
2024-12-12 16:49:33 +0100

    selected simulation panel

---
+++ engine/src/scenes/simulationManagement/SelectedSimulation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Godot;
+
+public partial class SelectedSimulation: Panel
+{
+    [ExportGroup("DO NOT EDIT THESE")]
+    [Export] public Button FileManagerButton;
+    [Export] public Button CloseButton;
+    [Export] public TextureRect SimulationScreenshot;
+    [Export] public Button SettingsButton;
+    [Export] public Button DeleteButton;
+    [Export] public Button RunButton;
+    [Export] public Label NameLabel;
+    [Export] public Label DateTimeLabel;
+    [Export] public Label AgentCountLabel;
+    [Export] public Label TimeRunningLabel;
+    [Export] public FileDialog FileDialogWindow;
+
+    private String simulationPath;
+    
+    public override void _Ready()
+    {
+        this.FileManagerButton.Pressed += this.OnFileManagerButtonPressed;
+        this.CloseButton.Pressed += this.OnCloseButtonPressed;
+        this.SettingsButton.Pressed += this.OnSettingsButtonPressed;
+        this.DeleteButton.Pressed += this.OnDeleteButtonPressed;
+        this.RunButton.Pressed += this.OnRunButtonPressed;
+        
+        this.FileDialogWindow.FileMode = FileDialog.FileModeEnum.OpenFile;
+        this.FileDialogWindow.CurrentPath = simulationPath;
+        this.FileDialogWindow.GetOkButton().Disabled = true;
+        this.FileDialogWindow.GetCancelButton().Disabled = true;
+    }
+
+    private void OnFileManagerButtonPressed()
+    {
+        Vector2I size = (Vector2I)(GetViewport().GetVisibleRect().Size * 0.8f);
+        this.FileDialogWindow.PopupCentered(size);
+    }
+
+}
--- engine/src/scenes/simulationManagement/SimulationManagement.cs
+++ engine/src/scenes/simulationManagement/SimulationManagement.cs
@@ -3,13 +3,9 @@ using System;
 
 public partial class SimulationManagement : Control
 {
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-    }
+    [ExportGroup("DO NOT EDIT THESE")]
+    [Export]
+    public Panel SelectedSimulation;
+    
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
 }
